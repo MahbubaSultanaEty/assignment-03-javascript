b0190f92c1b263895042ec3da3f7105e84adb5e3
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-02-07 23:04:47 +0600

    problem 04 solved

---
+++ problem-05.js
@@ -0,0 +1,28 @@
+function analyzeText(str) {
+    // let longestWord = null;
+    let words = str.split(" ");
+    let longestWords = words[0];
+    // console.log(words);
+    for (let word of words) {
+        if (word.length > longestWords.length) {
+            longestWords = word;
+        }
+    } 
+    // console.log(longestWord);
+    let characters = str.split("");
+    // console.log(characters);
+    let token = 0;
+    for (let char of characters) {
+        if (char === ' ') {
+            continue
+        }
+        token++;
+    } // console.log(token);
+    return {
+        "longestWords": longestWords,
+        "token": token
+    }
+}
+
+let output= analyzeText("Keep coding keep shining")
+console.log(output);
